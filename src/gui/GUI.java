de1aba00d0705b1c4662ab35ed168be579dcc795
HP <HP@DESKTOP-IGBM6G9>
2021-02-13 11:41:31 -0600

    1.1

---
+++ src/gui/GUI.java
@@ -0,0 +1,34 @@
+/*
+ 
+ */
+package gui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HP
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.setTitle("Cesba");
+        
+        frame.setResizable(true);
+        // Formato normal:
+        //frame.getContentPane().setBackground(Color.GREEN);
+        // Formato RGB:
+        frame.getContentPane().setBackground(new Color(28, 26,198));
+        // Formato Exadecimal:
+       //frame.getContentPane().setBackground(Color ());
+        
+    }
+    
+}
